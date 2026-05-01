8e713aa6d7369ab482acc4785948b50f767c9f62
Aakashtyagi9889 <aakashtyagi9889@gmail.com>
2026-05-01 05:31:38 +0000

    Escape Sequence Character

---
+++ String/First.py
@@ -0,0 +1,11 @@
+# Escape Sequence Character
+
+# 1.  ==>  \n  (it create new line in output Terminal)
+intro  = "hello my name is Aakash Tyagi \n I am pursuing my MCA from RKGIT Collage"
+print(intro)
+
+# 2. ==> \t     (It create Tab Space )
+
+intro2 =  "hello my name is Aakash Tyagi \t I am pursuing my MCA from RKGIT Collage"
+print(intro2)
+
